f57725199e5158027fa8ae8e2e832a6a5fca69e5
Felobateer <felobateersamy01@gmail.com>
2025-03-06 18:55:44 +0100

    update

--- ng-walkthrough.d.ts
+++ ng-walkthrough.d.ts
@@ -1,1 +1,19 @@
-declare module 'ng-walkthrough';
+declare module 'ng-walkthrough' {
+  export interface WalkthroughStep {
+    selector: string;
+    description: string;
+    position?: 'top' | 'bottom' | 'left' | 'right';
+    arrow?: boolean;
+    arrowPosition?: 'top' | 'bottom' | 'left' | 'right';
+  }
+
+  export interface WalkthroughOptions {
+    steps: WalkthroughStep[];
+  }
+
+  export class NgWalkthroughService {
+    startWalkthrough(options: WalkthroughOptions): void;
+  }
+
+  export class NgWalkthroughModule {}
+}
