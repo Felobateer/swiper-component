a0dd30f678a9aee1f66302a748fdf666dca301b7
Felobateer <felobateersamy01@gmail.com>
2025-03-16 00:17:09 +0100

    add tests

--- projects/cyrano-showcase/src/lib/cyrano-showcase.component.spec.ts
+++ projects/cyrano-showcase/src/lib/cyrano-showcase.component.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {AngularWalkthroughComponent} from '../../../../src/app/components/angular-walkthrough/angular-walkthrough.component';
+import { CommonModule } from '@angular/common';
+import { Renderer2, ElementRef } from '@angular/core';
 
-import { CyranoShowcaseComponent } from './cyrano-showcase.component';
-
-describe('CyranoShowcaseComponent', () => {
-  let component: CyranoShowcaseComponent;
-  let fixture: ComponentFixture<CyranoShowcaseComponent>;
+describe('AngularWalkthroughComponent', () => {
+  let component: AngularWalkthroughComponent;
+  let fixture: ComponentFixture<AngularWalkthroughComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CyranoShowcaseComponent]
-    })
-    .compileComponents();
+      imports: [CommonModule],
+      declarations: [AngularWalkthroughComponent],
+      providers: [Renderer2, ElementRef]
+    }).compileComponents();
 
-    fixture = TestBed.createComponent(CyranoShowcaseComponent);
+    fixture = TestBed.createComponent(AngularWalkthroughComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should prepare walkthrough steps', () => {
+    component.titles = ['Step 1', 'Step 2', 'Step 3'];
+    component.prepareWalkthroughSteps();
+    expect(component.walkthroughSteps.length).toBe(3);
+    expect(component.walkthroughSteps[0].description).toBe('Step 1');
+  });
+
+  it('should apply transform on item click', () => {
+    component.titles = ['Step 1', 'Step 2', 'Step 3'];
+    component.prepareWalkthroughSteps();
+    component.onItemClick(1);
+    fixture.detectChanges();
+    const swiperList = fixture.nativeElement.querySelector('.swiper-list');
+    expect(swiperList.style.transform).toContain('translateX');
   });
 });
--- projects/cyrano-showcase/src/lib/cyrano-showcase.service.spec.ts
+++ projects/cyrano-showcase/src/lib/cyrano-showcase.service.spec.ts
@@ -1,16 +1,59 @@
 import { TestBed } from '@angular/core/testing';
+import { LangService } from '../../../../src/app/services/lang.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { Observable, of } from 'rxjs';
+import { Content } from '../../../../src/app/interfaces/content';
 
-import { CyranoShowcaseService } from './cyrano-showcase.service';
+class FakeLoader implements TranslateLoader {
+  getTranslation(lang: string): Observable<Content> {
+    return of({ slides: [] });
+  }
+}
 
-describe('CyranoShowcaseService', () => {
-  let service: CyranoShowcaseService;
+describe('LangService', () => {
+  let service: LangService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(CyranoShowcaseService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: FakeLoader }
+        })
+      ],
+      providers: [LangService]
+    });
+    service = TestBed.inject(LangService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should set and get language', () => {
+    service.setLang('fr');
+    expect(service.getLang()).toBe('fr');
+  });
+
+  it('should get translation', (done) => {
+    service.getTranslation('en').subscribe((translation) => {
+      expect(translation).toEqual({ slides: [] });
+      done();
+    });
+  });
+
+  it('should handle translation error', (done) => {
+    spyOn(service, 'getTranslation').and.returnValue(new Observable(observer => {
+      observer.error('Translation error');
+    }));
+
+    service.getTranslation('invalid').subscribe({
+      next: () => {},
+      error: (error) => {
+        expect(error).toBe('Translation error');
+        done();
+      }
+    });
   });
 });
